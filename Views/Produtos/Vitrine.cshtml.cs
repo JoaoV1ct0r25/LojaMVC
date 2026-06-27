0c76d4f5c6225df6aeb97e58847705f483af9671
João Victor <Joaoescapautos@gmail.com>
2026-06-27 20:46:47 -0300

    Implementando CRUD do Produtos e area publica/admin

---
+++ Views/Produtos/Vitrine.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LojaMVC.Views.Produtos
+{
+    public class VitrineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
